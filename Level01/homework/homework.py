95acb92629604dfd12de4413416a3ca630a7c7ab
Flamed TO <whydure@gmail.com>
2025-02-25 23:15:46 +0400

    GOA

---
+++ Level01/homework/homework.py
@@ -0,0 +1,7 @@
+name = "nika"
+surname = "quridze"
+age = 14
+height = 187
+ido= "programming and chess"
+
+print("hey, my name is",name,surname+", im",str(age),"years old,",str(height),"cm tall and i do",ido)
---
+++ Level02/homework/homework.py
@@ -0,0 +1,44 @@
+#mesame davaleba
+
+
+print(3/4)
+
+
+#meotxe davaleba
+
+name = "nika"
+surname = "quridze"
+age = 14
+print("Hello, my name is",name,surname+", i am",age,"years old.")
+
+
+
+#mexute davaleba
+print("sandro jojua")
+print("vaja sabauri")
+
+
+#meeqvse davaleba
+first_int = 1
+second_int = 2
+third_int = 3
+
+first_str = "one"
+second_str = "two"
+third_str = "three"
+
+first_float = 0.1
+second_float =0.2
+third_float = 0.3
+
+print (first_int)
+print (first_str)
+print (first_float)
+
+print (second_int)
+print (second_str)
+print (second_float)
+
+print(third_int)
+print(third_str)
+print(third_float)
