427a4704676d7a7faa774f3d1e2468f9e9b5825e
nquridze <140600149+nquridze@users.noreply.github.com>
2025-05-28 22:07:18 +0400

    Add files via upload

---
+++ Level12/homework/homework.py
@@ -0,0 +1,28 @@
+for i in range(30):
+    print("nika")
+
+
+num = 0
+for i in range(51):
+    print(num)
+    num +=1
+
+
+num1 = 25
+for i in range(76):
+    print(num1)
+    num1 +=1
+
+num2=5
+for i in range(13):
+    print(num2)
+    num2+=5
+
+num3=0
+for i in range(6):
+    print(num3)
+    num3+=150
+
+ragaca="smth"
+for i in ragaca:
+    print(i)
---
+++ Level14/homework/homework.py
@@ -0,0 +1,40 @@
+age=15
+name="nika"
+for i in range(age):
+    print(name)
+
+num=1
+for i in range(19):
+    print(num)
+    num+=1
+
+for i in range(20):
+    print(num)
+    num-=1
+
+num=10
+while num <=15:
+    print(num)
+    num += 1
+
+
+num=50
+while num !=0:
+    print(num)
+    num-=1
+
+
+#counter cvladi aris cvali, romelic loopshi mnishvnelobas imatebs an iklebs
+
+#while loops viyenebt mashin rodesac ar vicit loopi usasruloa tu ara
+
+num=10
+while num<=70:
+    print(num)
+    num+=2
+
+num=25
+while num<=55:
+    print(num)
+    num+=2
+
